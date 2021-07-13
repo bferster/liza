761c5b3f74b0a8d890c300e0be83cf566ed01178
bferster <bferster@stagetools.com>
2021-07-13 11:24:16 -0400

    more moduels

---
+++ three.js
@@ -0,0 +1,7 @@
+
+		import * as THREE from '/lib/three/build/three.module.js';
+		import { ColladaLoader } from '/lib/three/examples/jsm/loaders/ColladaLoader.js';
+		import { OrbitControls } from '/lib/three/examples/jsm/controls/OrbitControls.js';
+		export { THREE, ColladaLoader, OrbitControls };
+
+	
