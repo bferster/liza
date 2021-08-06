51b4212bb52e14c54623c20f523361e6f7b2fc7e
bferster <bferster@stagetools.com>
2021-08-06 16:17:43 -0400

    trait training

---
+++ authapp.js
@@ -0,0 +1,95 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// APP CLASS
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class App  {																					
+
+	constructor()   																			// CONSTRUCTOR
+	{
+		app=this;																					// App global pointer
+		this.ai=new AI();																			// Alloc AI class	
+		this.InitSpreadSheet();																		// Init data spreadsheet
+		this.sessionId=window.location.search.substring(1); 										// Set id
+		this.sd=[];																					// Session data
+this.sessionId="RER-1";
+		this.LoadSession();																			// Load curent session
+		}
+
+	InitSpreadSheet()																			// INIT DATA SPREADSHEET
+	{
+		this.pFields=[{ name:"type", type:"text", width:25, validate:"required"},					// CSV fields
+			{ name:"text", type:"text", width:100 },
+			{ name:"intent", type:"text", width:25 },
+			{ name:"topic", type:"text", width:25},
+			{ name:"traits", type:"text",width:50 },
+			{ name:"entities", type:"text",width:50 },
+			{ type:"control", width:0  }];
+
+		$("#jsGridData").jsGrid({ width:"100%", height:"calc(100vh - 168px)",						// Init JSGrid
+				inserting:true, editing:true, sorting:true,
+				fields: this.pFields, data:[],
+				onItemEditing: function(args) { app.curRow=args.itemIndex;  }
+				});
+		$("#jsGridData").css("padding",0)		
+	}
+
+	LoadSession(callback)																		// LOAD SESSION DATA
+	{
+		let url="https://viseyes.org/liza/config/loadsession.php?f="+this.sessionId+".json";		// Point at script
+		$.ajax({ url:url }).done((d)=> { 															// Load file
+			if (d) 			this.sd=JSON.parse(d);													// Objectify
+			$("#jsGridData").jsGrid("option","data",this.sd);										// Load into spreadsheet
+			if  (callback) 	callback() ;															// Run callback if set	
+			});		
+	}
+
+	SaveSession()																				// SAVE SESSION DATA
+	{
+		let url="https://viseyes.org/liza/config/savesession.php?f="+this.sessionId+".json";		// Point at script
+		this.sd=$("#jsGridData").jsGrid("option","data");											// Pull current state of data from spreadsheet
+		let data={ s:JSON.stringify(this.sd) };														// Stringify
+		$.ajax({ url:url, type: "POST", method:'POST', data:data })									// Write to file	
+			.done((s)=>{ 																			// Done
+				if (s == 0)	Sound("delete"),PopUp("Error saving session!");							// Send error 
+			 	else		Sound("ding"); 															// Success
+				app.ai.Train();																		// Train AI
+				})		
+	}
+
+	ExportSession()																				// EXPORT SESSION TO CSV
+	{
+		let fields=["type","text","intent","topic","traits","entities"];							// Fields
+		let data=Papa.unparse(this.sd,{ header:true, skipEmptyLines:true, columns:fields });		// Make CSV using lib
+		let textFileAsBlob=new Blob([data], {type:'text/plain'});									// Get blob
+		let downloadLink=document.createElement("a");												// Fake link
+		downloadLink.download=app.sessionId+".csv";													// Filename
+		downloadLink.innerHTML="Download File";														// Fake html														
+		downloadLink.href=window.URL.createObjectURL(textFileAsBlob);								// Payload
+		downloadLink.style.display="none";															// Hide
+		downloadLink.id="tdll";																		// Id
+		downloadLink.onclick=()=>{ downloadLink.remove(); };										// On click, remove fake link
+		document.body.appendChild(downloadLink);													// Add to DOM
+		downloadLink.click();																		// Trigger fake link
+	}
+	
+	ImportSession(e)																			// IMPORT SESSION FROM CSV
+	{	
+		let i,k;
+		let file=e.target.files[0];																	// Point at file
+		if (!file) 	return;																			// Quit if bad
+		let reader=new FileReader();																// Init reader
+		reader.readAsText(file);																	// Read file
+		reader.onload=(e)=>{ 																		// When loaded
+			this.sd=Papa.parse(e.target.result, { header:true, skipEmptyLines:true }).data;			// Parse CSV using papa lib
+			for (i=0;i<this.sd.length;++i)															// For each line
+				for (k in this.sd[i]) if (!k) delete this.sd[i][k];									// If empty field, delete it
+			Sound("ding");																			// Ding
+			};
+		$("#jsGridData").jsGrid("option","data",this.sd);											// Load into spreadsheet
+		}
+
+	Login()																						// LOGIN
+	{
+	}
+
+} // Class closure
--- witai.js
+++ witai.js
@@ -1,7 +1,11 @@
 class AI  {	
 	constructor()   																			// CONSTRUCTOR
 	{
-		this.token="HZNYC4A6QBJ5D6U6W4WDVGCIQIOQFADK";	
+		this.token="3ISBCQZSCQ37KJIZA7U2VFFSGEM75NDH";	
+		this.lut=[];
+		this.lut.bye="wit/bye";				this.lut.greetings="wit/greetings";
+		this.lut["on/off"]="wit/on_off";	this.lut.sentiment="wit/sentiment";
+		this.lut.thanks="wit/thanks";
 	}
 
 	Save(type)
@@ -18,6 +22,49 @@ class AI  {
 
 	Train()
 	{
+		let i,o;
+		for (i=0;i<app.sd.length;++i) {
+			o=app.sd[i];
+			if (o.type == "TRAIT")			this.SetTrait(this.lut[o.text] ? this.lut[o.text] : o.text, o.traits);
+//			else if (o.type == "ENTITY")	this.SetEntity(this.lut[o.text] ? this.lut[o.text] : o.text]);
+			else if (o.type == "INTENT")	this.SetIntent(this.lut[o.text]);
+			else if (o.type == "REMARK")	this.SetRemark(this.lut[o.text]);
+			}
 	}
+
+	SetTrait(trait, values)
+	{
+		if (trait.match(/wit\//)) return;
+		if (values)	values=values.replace(/\ /g,"").split(",");
+		let body={ name:trait, values};
+		this.SendCommand("traits", body);
+	}
+
+	SetEntity(entity)
+	{
+		if (entity.match(/wit\//)) return;
+		let body={ name:entity, role:entity.replace(/wit\//,"") };
+		this.SendCommand("entitities", body);
+	}
+	
+	SetRemark(remark)
+	{
+	}
+
+	SetIntent(intent)
+	{
+	}
+
+	SendCommand(type, body)
+	{
+		const url="https://api.wit.ai/"+type+"?v=20210806"
+		fetch(url,{ method:"POST",
+			  headers: { Authorization:'Bearer '+this.token, 'Content-Type':'application/json'}, 
+			  body: JSON.stringify(body)
+			  })	
+	  	.then(res => res.json())
+	  	.then(res => console.log(res))
+	}
+
 
 } // AI class closure
