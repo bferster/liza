6b6857c2adf790b980df953c57c07b4d7d3df8a6
bferster <bferster@stagetools.com>
2021-08-06 18:17:29 -0400

    intents lut

--- witai.js
+++ witai.js
@@ -3,9 +3,19 @@ class AI  {
 	{
 		this.token="3ISBCQZSCQ37KJIZA7U2VFFSGEM75NDH";	
 		this.lut=[];
-		this.lut.bye="wit/bye";				this.lut.greetings="wit/greetings";
-		this.lut["on/off"]="wit/on_off";	this.lut.sentiment="wit/sentiment";
+		this.lut.bye="wit/bye";					this.lut.greetings="wit/greetings";
+		this.lut.onoff="wit/on_off";			this.lut.sentiment="wit/sentiment";
 		this.lut.thanks="wit/thanks";
+
+		this.lut.who="wit/who";						this.lut.age="wit/age";		
+		this.lut.money="wit/amount_of_money";		this.lut.contact="wit/contact";		
+		this.lut.creative="wit/creative";			this.lut.date="wit/date_time";
+		this.lut.distance="wit/distance";			this.lut.duration="wit/duration";
+		this.lut.location="wit/location";			this.lut.math="wit/math_expression";
+		this.lut.notable_person="celebrity";		this.lut.number="wit/number";
+		this.lut.ordinal="wit/ordinal";				this.lut.quantity="wit/quantity";
+		this.lut.reminder="wit/reminder";			this.lut.temperature="wit/temperature"
+		this.lut.volume="wit/volume";
 	}
 
 	Save(type)
@@ -32,12 +42,11 @@ class AI  {
 			}
 	}
 
-	SetTrait(trait, values)
+	SetTrait(trait)
 	{
 		if (trait.match(/wit\//)) return;
-		if (values)	values=values.replace(/\ /g,"").split(",");
-		let body={ name:trait, values};
-		this.SendCommand("traits", body);
+		let body={  name:"phase", values:[] };
+ 		this.SendCommand("traits", body);
 	}
 
 	SetEntity(entity)
@@ -57,6 +66,7 @@ class AI  {
 
 	SendCommand(type, body)
 	{
+		trace(body)
 		const url="https://api.wit.ai/"+type+"?v=20210806"
 		fetch(url,{ method:"POST",
 			  headers: { Authorization:'Bearer '+this.token, 'Content-Type':'application/json'}, 
