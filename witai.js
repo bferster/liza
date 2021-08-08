e07e7fe092af00804ee39fd99c790931793ebf18
bferster <bferster@stagetools.com>
2021-08-08 18:22:08 -0400

    entities synonymns

--- witai.js
+++ witai.js
@@ -3,18 +3,17 @@ class AI  {
 	{
 		this.token="3ISBCQZSCQ37KJIZA7U2VFFSGEM75NDH";	
 		this.lut=[];
-		this.lut.bye="wit/bye";					this.lut.greetings="wit/greetings";
-		this.lut.onoff="wit/on_off";			this.lut.sentiment="wit/sentiment";
+		this.lut.bye="wit/bye";							this.lut.greetings="wit/greetings";
+		this.lut.onoff="wit/on_off";					this.lut.sentiment="wit/sentiment";
 		this.lut.thanks="wit/thanks";
 
-		this.lut.who="wit/who";						this.lut.age="wit/age";		
-		this.lut.money="wit/amount_of_money";		this.lut.contact="wit/contact";		
-		this.lut.creative="wit/creative";			this.lut.date="wit/date_time";
-		this.lut.distance="wit/distance";			this.lut.duration="wit/duration";
-		this.lut.location="wit/location";			this.lut.math="wit/math_expression";
-		this.lut.notable_person="celebrity";		this.lut.number="wit/number";
-		this.lut.ordinal="wit/ordinal";				this.lut.quantity="wit/quantity";
-		this.lut.reminder="wit/reminder";			this.lut.temperature="wit/temperature"
+		this.lut.money="wit/amount_of_money";			this.lut.contact="wit/contact";		
+		this.lut.creative="wit/creative";				this.lut.date="wit/date_time";
+		this.lut.distance="wit/distance";				this.lut.duration="wit/duration";
+		this.lut.location="wit/location";				this.lut.math="wit/math_expression";
+		this.lut.notable_person="wit/notable_person";	this.lut.number="wit/number";
+		this.lut.ordinal="wit/ordinal";					this.lut.quantity="wit/quantity";
+		this.lut.reminder="wit/reminder";				this.lut.temperature="wit/temperature"
 		this.lut.volume="wit/volume";
 	}
 
@@ -36,8 +35,8 @@ class AI  {
 		for (i=0;i<app.sd.length;++i) {
 			o=app.sd[i];
 			if (o.type == "TRAIT")			this.SetTrait(this.lut[o.text] ? this.lut[o.text] : o.text, o.traits);
-//			else if (o.type == "ENTITY")	this.SetEntity(this.lut[o.text] ? this.lut[o.text] : o.text);
-			else if (o.type == "INTENT")	this.SetIntent(o.intent);
+			else if (o.type == "ENTITY")	this.SetEntity(this.lut[o.text] ? this.lut[o.text] : o.text,o.traits);
+//			else if (o.type == "INTENT")	this.SetIntent(o.intent);
 //			else if (o.type == "REMARK")	this.SetRemark(this.lut[o.text]);
 			}
 	}
@@ -49,13 +48,17 @@ class AI  {
  		this.SendCommand("traits", body);
 	}
 
-	SetEntity(entity)
+	SetEntity(entity, traits)																	// SEND ENTITIES TO AI
 	{
-//		if (entity.match(/wit\//)) return;
-		let body={ name:entity, roles:[] };
-		trace(body)
-		
-		this.SendCommand("entities", body);
+		let i,keywords=[];
+		if (traits) {																				// If any keywords defined
+			traits=traits.replace(/ /g,"");															// Remove spaces
+			traits=traits.split(",");																// Put into array
+			for (i=0;i<traits.length;++i)															// For each keyword
+				keywords.push( { "keyword": traits[i], "synonyms":[ traits[i] ]} );					// Add 
+			}
+		let body={ name:entity, roles:[], keywords:keywords };										// Make payload
+		this.SendCommand("entities", body);															// Send to wit.ai
 	}
 	
 	SetRemark(remark)
