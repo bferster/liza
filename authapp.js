035211f39872851d795ba45e557a2b2862225601
bferster <bferster@stagetools.com>
2021-08-11 17:52:52 -0400

    remarks editor

--- authapp.js
+++ authapp.js
@@ -95,25 +95,39 @@ this.sessionId="RER-1";
 	
 	RemarksEditor(type)																			// EDIT REMARKS AND RESPONSES
 	{
+		let i,o,entval="";
+		let traits=[],entities=["Choose entitity"],intents=["None"],students=["Whole class"];
+		let myEntities=["student:Luis"], myTraits=["Sentiment:POSITIVE"]
+		for (i=0;i<this.sd.length;++i) {															// For each item
+			o=this.sd[i];																			// Point at it
+			if (o.type == "TRAIT")		  traits.push(o.text);										// Add trait
+			else if (o.type == "ENTITY")  entities.push(o.text);									// Add entity
+			else if (o.type == "INTENT")  intents.push(o.intent+" - "+o.text);						// Add intent
+			else if (o.type == "STUDENT") students.push(o.text);									// Add student
+			}
+
 		let text,curItem=0,maxItems=10;
 		let str=`<div class="lz-remarks">
-		<p>${type.toUpperCase()}: <span id="${type}Counter"/></b></p>
-		<table style="width:50%;max-width:800px;margin:0 auto;text-align:right">
+		<p><b>${type.toUpperCase()}: <span id="${type}Counter"/></b></p>
+		<table style="width:100%;max-width:1200px;margin:0 auto;text-align:right">
 			<tr><td style="float:right;margin-right:-6px"><img id="${type}LeftArrow" src="img/arrowleft.png" class="lz-arrow"></td>
 			<td><input id="${type}Text" type="text" class="lz-mainText"></td>
 			<td><img style="float:left" id="${type}RightArrow" src="img/arrowright.png" class="lz-arrow"></td></tr>
 			<tr><td>INTENT: &nbsp;</td><td><select class="lz-is" style="width:calc(100% + 6px)" id="${type}Intent"></select></td><td></td></tr>
+			<tr><td>STUDENT: &nbsp;</td><td><select class="lz-is" style="width:calc(100% + 6px)" id="${type}Student"></select></td><td></td></tr>
 			<tr><td>TOPIC: &nbsp;</td><td><select class="lz-is" style="width:calc(100% + 6px)" id="${type}Topic"></select></td><td></td></tr>
-			<tr><td>ENTITIES: &nbsp;</td><td style="text-align:center;border:1px solid #999;border-radius:12px" id="${type}Entities"></td><td></td></tr>
-				<tr><td>TRAITS: &nbsp;</td><td style="text-align:center;border:1px solid #999;border-radius:12px" id="${type}Traits"></td><td></td></tr>
-
+			<tr><td>ENTITIES: &nbsp;</td><td style="text-align:center;border:1px solid #999;border-radius:12px;padding:8px;background-color:#d5e8f3" id="${type}Entities"></td><td></td></tr>
+			<tr><td>TRAITS: &nbsp;</td>	<td style="text-align:center;border:1px solid #999;border-radius:12px;padding:8px;background-color:#d5efd2" id="${type}Traits"></td><td></td></tr>
 			</table>
 		</div>`;
+
 		$("#"+type+"sEditor").html(str.replace(/\t|\n|\r/g,""));									// Add to div
 		Draw();																						// Draw dynamic data
 	
-		$("#"+type+"Intent").append("<option>None</option>");
-		$("#"+type+"Intent").append("<option>110.1 Giving feeedback to student</option>");
+		for (i=0;i<intents.length;++i)																// For each intent
+			$("#"+type+"Intent").append("<option>"+intents[i]+"</option>");							// Add it
+		for (i=0;i<students.length;++i)																// For each student
+			$("#"+type+"Student").append("<option>"+students[i]+"</option>");						// Add it
 																									
 		$("#"+type+"LeftArrow").on("click", ()=>{													// ON LEFT ARROW
 			curItem=Math.max(--curItem,0);  														// Decrement													  
@@ -126,14 +140,37 @@ this.sessionId="RER-1";
 
 		function Draw()																				// DRAW DYNAMIC DATA
 		{
+			let str="";
 			text="How did you come up with that answer? "+curItem;									// Get text
 			$("#"+type+"Text").val(text);															// Set text
 			$("#"+type+"Counter").html(curItem);													// Set count
-			let str="<b>STUDENT</b>: Luis<span style='float:right'>x&nbsp;</span>"	
-			$("#"+type+"Entities").html(str)		
-			str="<b>SENTIMENT</b>: POSITIVE";	
+			for (i=0;i<myEntities.length;++i) {														// For each entity coded
+				str+=`<b>${myEntities[i].split(":")[0].toUpperCase()}</b> : ${myEntities[i].split(":")[1]}
+				<img src="img/trashbut.gif" style="cursor:pointer;float:right"><br style="clear:both">`;
+				}
+			str+=`<br><select class="lz-is" style="margin-left:8px;width:150px;height:22px;display:none" placeholder="choose" id="${type}NewEntity">`;
+			for (i=0;i<entities.length;++i) str+="<option>"+entities[i]+"</option>";				// Add entities
+			str+="</select>" 
+			$("#"+type+"Entities").html(str);														// Add markup		
+			str="<b>SENTIMENT</b> : POSITIVE";	
 		
-			$("#"+type+"Traits").html(str)		
+			$("#"+type+"Traits").html(str);		
+			
+			$("#"+type+"NewEntity").on("change", ()=>{
+				$("#"+type+"NewEntity").css("display","none")	
+				myEntities.push($("#"+type+"NewEntity").val()+":"+entval.trim());
+				Draw();																				// Redraw
+				});
+			
+			$('input').mouseup(function() {
+				if (window.getSelection) 	 entval=window.getSelection().toString();
+				else if (document.selection) entval=document.selection.createRange().text;
+				$("#"+type+"NewEntity").css("display","block");
+				$("#"+type+"NewEntity").val("Choose entitity")
+
+			});
+		
+
 		}
 
 	}
