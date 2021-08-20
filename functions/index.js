367c794b273ca7df3d79d9895ae921274907d000
bferster <bferster@stagetools.com>
2021-08-20 17:35:09 -0400

    intial querry

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,13 @@ const dialogflow = require("dialogflow");
 const uuid = require("uuid");
 
 	exports.helloWorld = functions.https.onRequest((request, response) => {
-		functions.logger.info("Hello logs!", {structuredData: true});
-		response.send("Hello from Firebase ok?");
-		});
 
 
 	async function runSample() {
 	console.log("Starting now");	
 		const sessionId=uuid.v4();	   													// A unique identifier for the given session
 	   	const sessionClient = new dialogflow.SessionsClient({ 							// Create a new session
-			keyFilename: "../jokes-68aad-358acba1b510.json"								// Token path
+			keyFilename: "jokes-68aad-358acba1b510.json"								// Token path
 	   		});
 	   const sessionPath = sessionClient.sessionPath("jokes-68aad", sessionId);			// Session path
 		 
@@ -20,7 +17,7 @@ const uuid = require("uuid");
 		 session: sessionPath,
 		 queryInput: {
 		   text: {
-			 text: "Chris. How about you. Why don't you read your story",
+			 text: "Chris. You're a bad guy",
 			 languageCode: "en-US"
 		   	}
 		 }
@@ -28,18 +25,21 @@ const uuid = require("uuid");
 	 
 		const responses = await sessionClient.detectIntent(request);  				// Send request and log result
 		console.log("Detected intent");
-		console.log(responses);
-		const result = responses[0].queryResult;
+	const result = responses[0].queryResult;
+		console.log(result);
 		console.log(`  Query: ${result.queryText}`);
-		console.log(`  Response: ${result.fulfillmentText}`);
+		console.log(`  Entities: ${JSON.stringify(result.parameters.fields)}`);
 		if (result.intent) 
 			console.log(`  Intent: ${result.intent.displayName}`);
 		else 
 			console.log(`  No intent matched.`);
+			response.send(responses);
+
 		}
 	 
 	 runSample();
 
+		});
 	
 	
 	
