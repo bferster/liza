b793394e957d047da058a16c6e354a856b3bd56c
bferster <bferster@stagetools.com>
2021-10-10 10:00:20 -0400

    old files culled

--- three.js
+++
@@ -1,7 +0,0 @@
-
-		import * as THREE from '/lib/three/build/three.module.js';
-		import { ColladaLoader } from '/lib/three/examples/jsm/loaders/ColladaLoader.js';
-		import { OrbitControls } from '/lib/three/examples/jsm/controls/OrbitControls.js';
-		export { THREE, ColladaLoader, OrbitControls };
-
-	
